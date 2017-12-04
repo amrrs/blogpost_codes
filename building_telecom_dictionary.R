84defe5fda962cf79b1d843e7311f80289ceb067
amrrs <amrrs@outlook.com>
2017-12-04 19:42:24 +0530

    final update

--- building_telecom_dictionary.R
+++ building_telecom_dictionary.R
@@ -6,9 +6,11 @@ library(stringr)
 #url whose content to be scraped
 
 url <- 'http://www.atis.org/glossary/definitionsList.aspx?find=A&kw=0'
+#url <- 'http://www.atis.org/glossary/definitionsList.aspx?find=B&kw=0'
 
 #extracting the content of the given url
-url_content <- read_html('http://www.atis.org/glossary/definitionsList.aspx?find=A&kw=0')
+#url_content <- read_html('http://www.atis.org/glossary/definitionsList.aspx?find=A&kw=0')
+url_content <- read_html(url)
 
 
 #extracting all the links from the page 
@@ -20,9 +22,10 @@ text <- url_content %>% html_nodes('a') %>% html_text()
 #creating a new dictonary of links and text extracted above
 rough_dict <- data.frame(links,text, stringsAsFactors = F)
 
+head(rough_dict)
+
 #filtering glossary terms leaving out irrelevant information
 fair_dict <- rough_dict %>% filter(str_detect(links, 'id')) %>% select(text)
 
-
-fair_dict
+tail(fair_dict)
 
